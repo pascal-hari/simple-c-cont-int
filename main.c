5f5890dd88393e735680ce098c71eebf69bb8983
Pascal Hari <pascal.hari@csa.ch>
2018-01-24 16:30:28 +0100

    bl

--- main.c
+++ main.c
@@ -2,6 +2,6 @@
 int main()
 {
    // printf() displays the string inside quotation
-   printf("Hello, World! Your Jenkins setup seems to work. \nThis build happend after a simple git push. \n");
+   printf("dHello, World! Your Jenkins setup seems to work. \nThis build happend after a simple git push. \n");
    return 0;
 }
