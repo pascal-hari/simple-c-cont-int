469dc2c47938f476c144377a531aff57a10db77a
Pascal Hari <pascal.hari@csa.ch>
2018-01-25 15:35:56 +0100

    fixed bug in outputtext

--- main.c
+++ main.c
@@ -2,8 +2,6 @@
 int main()
 {
    // printf() displays the string inside quotation
-   printf("Hello, World! Your Jenkins setup seems to work. \nThis build happend after hitting the right URL. \nThere is no 
-git hook for post-push so we need to manually hit the URL. \nIn a real enviroment this could be done on the Git-Server. 
-But our Git server is external and doesnt have acess to localhost.\n");
+   printf("Hello, World! Your Jenkins setup seems to work. \nThis build happend after hitting the right URL. \nThere is no git hook for post-push so we need to manually hit the URL. \nIn a real enviroment this could be done on the Git-Server. But our Git server is external and doesnt have acess to localhost.\n");
    return 0;
 }
